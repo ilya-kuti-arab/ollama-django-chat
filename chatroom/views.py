878b4297812fdc102c3fe0a13e29ce9878c295cd
ilya <eliyakooti@gmail.com>
2026-05-10 06:58:08 -0700

    fix utils in views.py

--- chatroom/views.py
+++ chatroom/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-from utils import call_ollama_model
+from .utils import call_ollama_model
 from requests import post
 from json import loads, JSONDecodeError
 
